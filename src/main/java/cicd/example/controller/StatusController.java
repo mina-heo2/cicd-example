e1e4f806e328f31b7dc982f647bbdb28fb2b2076
mina-heo2 <minah.tmax@gmail.com>
2024-08-08 13:59:58 +0900

    Create StatusController.java

---
+++ src/main/java/cicd/example/controller/StatusController.java
@@ -0,0 +1,27 @@
+package devOps.practice.CI.CD.Practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @GetMapping("/status")
+    public String checkStatus() {
+        return "WebHook Test";
+    }
+
+    @GetMapping("/status/v2")
+    public String checkStatusV2() {
+        return "WebHook Test2";
+    }
+
+    @GetMapping("/status/v3")
+    public String checkStatusV3() {
+        return "WebHook Test3";
+    }
+
+    @GetMapping("/status/v4")
+    public String checkStatusV4() {
+        return "WebHook Test4";
+    }
+}
