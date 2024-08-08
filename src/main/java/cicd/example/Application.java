9377bd09d4a456e41fc17ea313157aad6c25f5b0
mina-heo2 <minah.tmax@gmail.com>
2024-08-08 14:21:31 +0900

    Update Application.java

--- src/main/java/cicd/example/Application.java
+++ src/main/java/cicd/example/Application.java
@@ -1,4 +1,4 @@
-package devOps.practice.CI.CD.Practice;
+package devops.example.ci.cd.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
